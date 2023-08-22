bd91b7a052e69187e94ed2106ddd8ae2fc8413e3
SheriffedPerish <sheriffedPerish@gmail.com>
2023-08-22 13:56:06 +0200

    Full Flutter Project

---
+++ lib/main.dart
@@ -0,0 +1,50 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:layout/layout.dart';
+
+import 'firebase_options.dart';
+import 'pages/home/home_page.dart';
+import 'utils/configure_web.dart';
+import 'utils/routes.dart';
+import 'utils/values/app_theme.dart';
+import 'utils/values/values.dart';
+
+// Home
+// TODO: Add an Animation to the White Circle
+
+// Text
+// TODO: Fix Text Background Color
+// TODO: Make Text properly Selectable
+
+// Animations
+// TODO: Adjust the Curves
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  configureApp();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  runApp(const Website());
+}
+
+class Website extends StatelessWidget {
+  const Website({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Layout(
+      child: GetMaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: StringConst.appTitle,
+        theme: AppTheme.lightThemeData,
+        initialRoute: HomePage.homePageRoute,
+        onGenerateRoute: RouteConfiguration.onGenerateRoute,
+        home: const HomePage(),
+      ),
+    );
+  }
+}
