004251981f55e652a409400840f106d159149049
burak-basci <burakbasci98@gamil.com>
2026-05-18 22:35:02 +0200

    deploy: scroll fix on all pages, cascade outro, footer underline align

--- android/app/src/main/kotlin/com/example/burak_basci_website/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.burak_basci_website
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- lib/utils/intro_redirect_stub.dart
+++
@@ -1,3 +0,0 @@
-/// Non-web stub: there is no URL bar to read or rewrite outside the browser,
-/// so capturing a deep link is a no-op.
-void captureDeepLinkInto(void Function(String?) setter) {}
