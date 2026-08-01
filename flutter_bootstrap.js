906956a7b50412464c98f97414e911e2027d683f
burak-basci <burakbasci98@gamil.com>
2026-08-01 09:33:48 +0000

    deploy: compositor-driven hero bob + bottom-anchored plane loop
    Source: 19f5502 on wip-redesign-source-2026-05-18.

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"a10d8ac38de835021c8d2f920dbf50a920ccc0
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "4035140198" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "3653214406" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
